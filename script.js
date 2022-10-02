8608daed30b4e41b1fba76bee91dc2b305d177ba
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-03 01:57:31 +0600

    Worked on edit button and fixed the bugs

--- script.js
+++ script.js
@@ -10,20 +10,15 @@ const form = document.querySelector("form");
 const deleteElement = document.querySelector(".btn-delete");
 const body = document.querySelector(".body");
 const msg = document.querySelector(".msg");
+const data = {};
 
+// Event Listener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   inputValidation();
 });
 
-const deleteBook = (e) => {
-  e.parentElement.parentElement.remove();
-};
-
-const editBook = (e) => {
-  e.parentElement.parentElement.remove();
-};
-
+// Input Validation
 const inputValidation = () => {
   if (title.value === "" && author.value === "") {
     msg.innerHTML = "Information cannot be blank";
@@ -31,23 +26,27 @@ const inputValidation = () => {
     msg.innerHTML = "";
     acceptData();
     displayUI();
+
     add.setAttribute("data-bs-dismiss", "modal");
-    add.click();
+    add.click(); //Fake click
+
+    //IIFE function for setting data-bs-dismiss empty
     (() => {
       add.setAttribute("data-bs-dismiss", "");
     })();
   }
 };
 
-const data1 = {};
+// Accept the data and store that into the data object
 const acceptData = () => {
-  data1["title"] = title.value;
-  data1["author"] = author.value;
-  data1["date"] = date.value;
-  data1["pages"] = pageNumber.value;
-  data1["check"] = bookCheck;
+  data["title"] = title.value;
+  data["author"] = author.value;
+  data["date"] = date.value;
+  data["pages"] = pageNumber.value;
+  data["check"] = bookCheck;
 };
 
+// Display or generate the book details into the UI
 const displayUI = () => {
   main.innerHTML += `<div class="card m-3" style="width: 18rem">
       <div class="card-body">
@@ -56,25 +55,25 @@ const displayUI = () => {
           <ul class="list-group">
             <li class="list-group-item">
               <span class="text-muted">Title:</span>
-              <span class="title">${data1.title}</span></li
+              <span class="title">${data.title}</span></li
             >
             <li class="list-group-item">
               <span class="text-muted">Author:</span>
-              <span class="author">${data1.author}</span>
+              <span class="author">${data.author}</span>
             </li>
             <li class="list-group-item">
               <span class="text-muted">Published:</span>
-              <span class="date">${data1.date}</span>
+              <span class="date">${data.date}</span>
             </li>
             <li class="list-group-item">
               <span class="text-muted">Pages:</span>
-              <span class="pages">${data1.pages}</span></li
+              <span class="pages">${data.pages}</span></li
             >
             <li class="list-group-item">
               <span class="text-muted"
                 >Read:
                 ${
-                  data1.check.checked
+                  data.check.checked
                     ? '<i class="fa-sharp fa-solid fa-check text-success"></i>'
                     : '<i class="fa-sharp fa-solid fa-xmark text-danger"></i>'
                 }</span
@@ -85,5 +84,24 @@ const displayUI = () => {
         <a href="#" onClick="deleteBook(this)" class="btn btn-primary btn-sm ms-3 btn-delete">Delete</a>
       </div>
     </div>`;
+
+  // Reset all the form content
   form.reset();
+};
+
+// Delete the Book from Library
+const deleteBook = (e) => {
+  e.parentElement.parentElement.remove();
+};
+
+// Edit the existing book details
+const editBook = (e) => {
+  e.parentElement.parentElement.remove();
+  let selectedElement = e.parentElement.children[1].children[0];
+  console.dir(selectedElement);
+  title.value = selectedElement.children[0].children[1].innerHTML;
+  author.value = selectedElement.children[1].children[1].innerHTML;
+  date.value = selectedElement.children[2].children[1].innerHTML;
+  pageNumber.value = selectedElement.children[3].children[1].innerHTML;
+  bookCheck.value = selectedElement.children[4].children[1];
 };
